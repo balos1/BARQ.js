869404e0908ef64a721e8ca1c54aaeda0e34d1e0
Cody Balos <cjbalos@gmail.com>
2015-05-22 00:17:10 -0700

    removed node_modules from repo

--- node_modules/qrcode/b.js
+++
@@ -1,9 +0,0 @@
-
-var top = new Buffer("GyMzYWIK",'base64');
-var bottom = new Buffer("GyM0YWIK",'base64');
-
-console.log(top);
-console.log(bottom);
-
-process.stdout.write(top.toString());
-process.stdout.write(bottom.toString()+"\n");
--- node_modules/qrcode/test/cli.js
+++
@@ -1,5 +0,0 @@
-var QRCode = require(__dirname+'/../qrcode.js');
-
-QRCode.drawAscii('yo yo yo',function(error,data){
-    console.log(data);
-});
